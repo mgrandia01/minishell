c8e829ff59c42fd626282f9535ae1023fbe32515
Marta Grandia Gutierrez <mgrandia@cbr4s5.42barcelona.com>
2025-08-30 17:11:53 +0200

    signals normi

--- srcs/signals.c
+++ srcs/signals.c
@@ -6,72 +6,30 @@
 /*   By: arcmarti <arcmarti@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 14:03:55 by arcmarti          #+#    #+#             */
-/*   Updated: 2025/08/30 10:34:04 by mgrandia         ###   ########.fr       */
+/*   Updated: 2025/08/30 17:11:26 by mgrandia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
-/*
-void	cleanup_outfiles(t_cmd *cmd)
-{
-	int i;
-
-	if (!cmd || !cmd->outfile_name)
-		return;
-
-	for (i = 0; i < cmd->outfile_count; i++)
-		unlink(cmd->outfile_name[i]); // borra el archivo del sistema
-}
-*/
-
 
 void	ft_handle_sigint(int sig)
 {
-//printf("SIGNALS");
 	(void)sig;
 	rl_replace_line("", 0);
 	write(0, "\n", 1);
-	//rl_replace_line(" ", STDOUT_FILENO);
-	rl_on_new_line();		// resetea línea interna de readline
-	//rl_replace_line("CTRL C", 0);		// limpia línea actual
-	rl_redisplay();			// vuelve a mostrar el prompt
-	//if (isatty(STDIN_FILENO))
-	//	ft_printf(STDOUT_FILENO, "\033[1;32mminishell \u25B8\033[0m ");
+	rl_on_new_line();
+	rl_redisplay();
 	g_exit_status = 130;
 }
 
 void	ft_handle_sigquit(int sig)
 {
-	//disable_sigquit_echo();
-	
 	(void)sig;
 	rl_replace_line("", 0);
-//	write(0, "\n", 1);
-	rl_on_new_line();                        // Resetea estado interno de readline
-	rl_redisplay();                          // Vuelve a mostrar el prompt	
-
-
-//	write(STDOUT_FILENO, "\b\b  \b\b", 6);// Borra visualmente dos caracteres
-	// cuidado con usar tgetstr(), tgoto() y tputs() pq pueden interferir con readline
+	rl_on_new_line();
+	rl_redisplay();
 }
 
-/*
-void	ft_handle_sigquit(int sig)
-{
-	//disable_sigquit_echo();
-	printf("slash\n");fflush(0);
-	
-	(void)sig;
-	rl_on_new_line();                        // Resetea estado interno de readline
-//	write(0, "\n", 1);
-	rl_replace_line("", 0);
-	rl_redisplay();                          // Vuelve a mostrar el prompt	
-
-
-//	write(STDOUT_FILENO, "\b\b  \b\b", 6);// Borra visualmente dos caracteres
-	// cuidado con usar tgetstr(), tgoto() y tputs() pq pueden interferir con readline
-}
-*/
 void	display_new_line(int sig)
 {
 	(void)sig;
@@ -79,16 +37,13 @@ void	display_new_line(int sig)
 	rl_on_new_line();
 }
 
-// SIGINT CTRL + C
-// SIGQUIT CTRL + contrabrarra
 void	ft_setup_signals(int enable)
 {
-	if (enable ==  1)
+	if (enable == 1)
 	{
 		signal(SIGINT, ft_handle_sigint);
 		disable_sigquit_echo();
 		signal(SIGQUIT, ft_handle_sigquit);
-
 	}
 	else if (enable == 2)
 	{
@@ -97,9 +52,7 @@ void	ft_setup_signals(int enable)
 	}
 	else
 	{
-	signal(SIGINT, display_new_line);
-	signal(SIGQUIT, display_new_line);
-		//signal(SIGINT, SIG_IGN);
-	//	signal(SIGQUIT, SIG_IGN);
+		signal(SIGINT, display_new_line);
+		signal(SIGQUIT, display_new_line);
 	}
 }
